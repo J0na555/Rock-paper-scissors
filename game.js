59a3add019abf11fcefb603ced7eaad635d19ad7
J0na555 <ethiopediaa@gmail.com>
2025-05-15 22:16:49 +0300

    the first commit of this game

---
+++ game.js
@@ -0,0 +1,50 @@
+// the game logic
+
+const choices = ["rock", "paper", "scissors"];
+
+const playerScore = document.getElementById("player_choice")
+const computerScore = document.getElementById("computer_choice")
+const result = document.getElementById("score")
+const body = document.body
+
+const playgame = (player_choice) => {
+    const computer_choice = choices[Math.floor(Math.random() * choices.length)]
+    body.classList.remove("greenBackground", "redBackground", "brownBackground")
+    
+    let score = ""
+
+    if(player_choice === computer_choice){
+        score = "It's a tie!"
+    }
+    else{
+        switch(player_choice){
+            case "rock":
+                score = computer_choice === "scissors" ? "You win!" : "You lose!"
+                break
+            case "paper":
+                score = computer_choice === "rock" ? "You win!" : "You lose!"
+                break
+            case "scissors":
+                score = computer_choice === "paper" ? "You win!" : "You lose!"
+                break
+        }
+    }
+
+    playerScore.textContent = `Player: ${player_choice}`;
+    computerScore.textContent = `Computer: ${computer_choice}`;
+    result.textContent = score
+
+    switch(score){
+        case "You win!":
+            body.classList.add("greenBackground") 
+            break
+        case "You lose!":
+            body.classList.add("redBackground")
+            break
+        case "It's a tie!":
+            body.classList.add("brownBackground")
+            break
+                
+            
+    }
+}
